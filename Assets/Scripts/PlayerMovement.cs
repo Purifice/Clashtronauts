bd884b0a74106f476de796e979b1fd4a6fc2db9a
Purifice <thepurificetwitch@gmail.com>
2023-12-15 12:00:11 -0700

    Added animations to character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     public bool isJumping;
     private bool facingRight = true;
 
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +26,8 @@ public class PlayerMovement : MonoBehaviour
         speed = 12.5f;
         jump = 20f;
         isJumping = false;
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,7 +36,6 @@ public class PlayerMovement : MonoBehaviour
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
 
-        
     }
 
     void FixedUpdate() //FOR PHYSICS
@@ -43,7 +46,13 @@ public class PlayerMovement : MonoBehaviour
             //The above movement method applies a force constantly as the key is pressed.
             //Not ideal for trying to have a constant max speed until I learn how to set the max velocity effectively
             rb2D.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb2D.velocity.y);
+            animator.SetBool("isWalking", true);
+
             //This above movement simply multiples the -1 / 1 response by the speed, so it is more linear
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
         }
 
         if (!isJumping && moveVertical > 0.1f)
@@ -51,7 +60,14 @@ public class PlayerMovement : MonoBehaviour
             // rb2D.AddForce(new Vector2(0f, moveVertical * jump), ForceMode2D.Impulse);
             //The above method uses the applying force method
             rb2D.velocity = new Vector2(rb2D.velocity.x, jump);
+            animator.SetBool("isJumping", true);
+
             //This method simply jumps by the jump amount linearly
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
+
         }
 
         if (moveHorizontal < 0 && facingRight)
@@ -71,20 +87,25 @@ public class PlayerMovement : MonoBehaviour
         {
             isJumping = false;
             speed = 12.5f;
+            animator.SetBool("isInAir", false);
 
         }
 
-       else if (collision.gameObject.tag == "Wall") //every frame upon the wall:
+        else if (collision.gameObject.tag == "Wall") //every frame upon the wall:
         {
 
             if (collision.gameObject.tag == "Surface") //every frame upon both the wall and surface
             {
                 isJumping = false;
                 speed = 12.5f;
+                animator.SetBool("isInAir", false);
+
             }
             else //every frame upon just the wall:
             {
                 speed = 0f;
+                animator.SetBool("isInAir", true);
+
             }
         }
     }
@@ -98,11 +119,13 @@ public class PlayerMovement : MonoBehaviour
             if (collision.gameObject.tag == "Wall") //if exiting both surface and wall
             {
                 isJumping = true;
+                animator.SetBool("isInAir", true);
                 speed = 10f;
             }
             else //for just exiting a collision with the surface, not the wall
             {
                 isJumping = true;
+                animator.SetBool("isInAir", true);
                 speed = 10f; 
             }
         }

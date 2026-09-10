b6de1461bb4b2b633306eb33c88aba5261883e98
Purifice <153021945+Purifice@users.noreply.github.com>
2026-09-09 18:31:56 -0600

    video settings
    - added video settings menu
    - added resolution select
    - added fullscreen select
    - added preference saving

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class PauseMenu : MonoBehaviour
@@ -12,23 +13,40 @@ public class PauseMenu : MonoBehaviour
     public GameObject pauseContainer;
     public GameObject optionsContainer;
     public GameObject audioContainer;
+    public GameObject videoContainer;
+
+
     public PlayerMovement playermovement;
+
     public bool isPaused;
+
     public Slider masterVolumeSlider;
     public Slider musicVolumeSlider;
     public Slider sfxVolumeSlider;
 
     private SoundMixerManager soundMixerManager;
 
+    public TMP_Dropdown resolutionDropdown;
+    public Toggle fullscreenToggle;
+
+    private List<Resolution> resolutions = new List<Resolution>();
+
     private void Start()
     {
         isPaused = false;
 
+        BuildResolutionDropdown();
+
         soundMixerManager = FindObjectOfType<SoundMixerManager>();
 
-        masterVolumeSlider.onValueChanged.AddListener(soundMixerManager.SetMasterVolume);
-        musicVolumeSlider.onValueChanged.AddListener(soundMixerManager.SetMusicVolume);
-        sfxVolumeSlider.onValueChanged.AddListener(soundMixerManager.SetSFXVolume);
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+        fullscreenToggle.onValueChanged.AddListener(delegate { FullScreenToggle(); });
+
+        masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
+        musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
+
+        LoadSettings();
     }
     void Update()
     {
@@ -47,6 +65,81 @@ public class PauseMenu : MonoBehaviour
         playermovement.menuAction = false;
     }
     
+
+    void LoadSettings()
+    {
+        int resolutionIndex = PlayerPrefs.GetInt("resolution");
+        SetResolution(resolutionIndex);
+
+        bool fullscreen = PlayerPrefs.GetInt("fullscreen", 1) == 1;
+
+        fullscreenToggle.isOn = fullscreen;
+        Screen.fullScreen = fullscreen;
+
+        float masterVolume = PlayerPrefs.GetFloat("masterVolume", 1f);
+        float musicVolume = PlayerPrefs.GetFloat("musicVolume", 1f);
+        float sfxVolume = PlayerPrefs.GetFloat("sfxVolume", 1f);
+
+        masterVolumeSlider.value = masterVolume;
+        musicVolumeSlider.value = musicVolume;
+        sfxVolumeSlider.value = sfxVolume;
+
+        soundMixerManager.SetMasterVolume(masterVolume);
+        soundMixerManager.SetMusicVolume(musicVolume);
+        soundMixerManager.SetSFXVolume(sfxVolume);
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        soundMixerManager.SetMasterVolume(value);
+        PlayerPrefs.SetFloat("masterVolume", value);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        soundMixerManager.SetMusicVolume(value);
+        PlayerPrefs.SetFloat("musicVolume", value);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        soundMixerManager.SetSFXVolume(value);
+        PlayerPrefs.SetFloat("sfxVolume", value);
+    }
+
+    public void FullScreenToggle()
+    {
+       
+          Screen.fullScreen = fullscreenToggle.isOn;
+          PlayerPrefs.SetInt("fullscreen", fullscreenToggle.isOn ? 1 : 0);
+   
+    }
+    void BuildResolutionDropdown()
+    {
+        resolutions.Clear();
+        List<string> options = new List<string>();
+        
+        foreach(Resolution res in Screen.resolutions)
+        {
+            if (resolutions.Exists(r => r.width == res.width && r.height == res.height)) continue;
+
+            resolutions.Add(res);
+            options.Add(res.width + " x " + res.height);
+        }
+
+        resolutionDropdown.ClearOptions();
+        resolutionDropdown.AddOptions(options);
+    }
+
+    public void SetResolution(int index)
+    {
+        Resolution res = resolutions[index];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+        resolutionDropdown.value = index;
+
+        PlayerPrefs.SetInt("resolution", index);
+    }
+
     public void ResumeButton()
     {
         //Debug.Log("Resume button clicked!");
@@ -80,7 +173,8 @@ public class PauseMenu : MonoBehaviour
 
     public void VideoButton()
     {
-
+        optionsContainer.SetActive(false);
+        videoContainer.SetActive(true);
     }
 
     public void ControlsButton()
@@ -90,8 +184,11 @@ public class PauseMenu : MonoBehaviour
 
     public void BackButton()
     {
+        PlayerPrefs.Save();
+
         optionsContainer.SetActive(false);
         audioContainer.SetActive(false);
+        videoContainer.SetActive(false);
         pauseContainer.SetActive(true);
     }
 }
